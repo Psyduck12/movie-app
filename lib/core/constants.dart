1d77e209dbd7f2374b80a835de00ef273a1ce353
Jeconiel Alexander <jecojason@gmail.com>
2022-02-02 16:10:26 +0700

    first commit

---
+++ lib/core/constants.dart
@@ -0,0 +1,5 @@
+const String baseUrl = 'https://api.themoviedb.org/3';
+const String apiKey = '';
+const String imageUrlLarge = 'https://image.tmdb.org/t/p/w500';
+const String imageUrlMedium = 'https://image.tmdb.org/t/p/w342';
+const String imageUrlSmall = 'https://image.tmdb.org/t/p/w185';
